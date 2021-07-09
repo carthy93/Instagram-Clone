03ca54c6488a9e61dd2ee48f9c8f608c4d2880c5
Karthik Balachandar <caarthik180@gmail.com>
2021-07-09 15:47:57 +0530

    JS added

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
- 
+const about = document.getElementsByClassName("profile-info")[0];
+
+const para = document.createElement("span");
+para.classList.add("info");
+
+para.innerHTML = `Bringing you closer to the people and things you love. ❤️`;
+para.style.fontSize = "21.55px";
+about.appendChild(para);
