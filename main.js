2940ea22f7407cf811d716f6848d710a489fe1fa
Karthik Balachandar <caarthik180@gmail.com>
2021-07-09 22:28:28 +0530

    JS added for modal

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ para.classList.add("info");
 para.innerHTML = `Bringing you closer to the people and things you love. ❤️`;
 para.style.fontSize = "21.55px";
 about.appendChild(para);
+
+const report = document.querySelector("#options");
+const modal = document.querySelector(".modal");
+
+// report.onclick = () => {
+//   modal.classList.add(".active");
+// };
+
+report.addEventListener("click", function () {
+  modal.classList.add("active");
+});
